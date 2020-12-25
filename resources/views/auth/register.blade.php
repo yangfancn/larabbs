90e84eef67e1768481de07c9c80a95a87c56ca06
yangfan <244190857@qq.com>
2020-12-25 15:13:22 +0800

    注册验证码

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -61,6 +61,19 @@
                             </div>
                         </div>
 
+                        <div class="form-group row">
+                          <label for="captcha" class="col-md-4 col-form-label text-md-right">验证码</label>
+                          <div class="col-md-6">
+                            <input type="text" id="captcha" class="form-control{{ $errors->has('captcha') ? ' is-invalid' : '' }}" name="captcha" required>
+                            <img src="{{ captcha_src('math') }}" title="点击重新获取" class="thumbnail captcha mt-3 mb-2" onclick="this.src='/captcha/math?' + Math.random()">
+                            @error('captcha')
+                              <span class="invalid-feedback" role="alert">
+                                  <strong>{{ $message }}</strong>
+                              </span>
+                            @enderror
+                          </div>
+                        </div>
+
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
