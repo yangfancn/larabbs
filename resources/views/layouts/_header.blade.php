52e47842b6f1c385a4be1361e94470d2df25dfa6
yangfan <244190857@qq.com>
2020-12-25 14:47:46 +0800

    修改登录成功跳转链接

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -14,8 +14,28 @@
       <!-- Right Side Of Navbar -->
       <ul class="navbar-nav navbar-right">
         <!-- Authentication Links -->
+        @guest
         <li class="nav-item"><a href="{{ route('login') }}" class="nav-link"><i class="fa fa-user mr-2"></i>登录</a></li>
         <li class="nav-item"><a href="{{ route('register') }}" class="nav-link">注册<i class="fa fa-arrow-right ml-2"></i></a></li>
+        @else
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navUserDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <img src="https://cdn.learnku.com/uploads/images/201709/20/1/PtDKbASVcz.png?imageView2/1/w/60/h/60" alt="" class="img-responsive img-circle" width="30px" height="30px">
+            {{ Auth::user()->name }}
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navUserDropdown">
+            <a href="#" class="dropdown-item">个人中心</a>
+            <a href="#" class="dropdown-item">编辑资料</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" id="logout" href="#">
+              <form action="{{ route('logout') }}" method="POST">
+                @csrf
+                <button class="logout text-muted" type="submit" name="button">退出<i class="fa fa-arrow-right ml-2"></i></button>
+              </form>
+            </a>
+          </div>
+        </li>
+        @endguest
       </ul>
     </div>
   </div>
