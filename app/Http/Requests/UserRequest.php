a708bcc0c2a94e0c8a87329ecacb0e5ffbb4aa02
yangfan <244190857@qq.com>
2020-12-30 18:51:51 +0800

    限制图片分辨率

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -28,6 +28,7 @@ class UserRequest extends FormRequest
             'name' => 'required|between:3,25|regex:/^[A-Za-z0-9\-\_]+$/|unique:users,name,' . Auth::id(),
             'email' => 'required|email|unique:users,email,' . Auth::id(),
             'introduction' => 'max:80',
+            'avatar' => 'mimes:png,jpg,jpeg,gif,bmp|dimensions:min_width=208,min_height=208',
         ];
     }
 
@@ -42,6 +43,8 @@ class UserRequest extends FormRequest
             'name.regex' => '用户名只支持英文、数字、横杠和下划线',
             'name.between' => '用户名长度介于3-25',
             'name.required' => '用户名不能为空',
+            'avatar.mimes' => '头像格式必须为 jpeg, bmp, png, gif',
+            'avatar.dimensions' => '图片最小分辨率为 208px * 208px',
         ];
     }
 }
